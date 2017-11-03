f10ea5edb4a78e0cf229210efc1248bde5dc7af3
Geordio <georgehaigh@hotmail.com>
2017-11-03 16:24:55 +0000

    minor tidy up

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -94,14 +94,13 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       Convert radar from polar to cartesian coordinates and initialize state.
       */
 
-
     	float rho = measurement_pack.raw_measurements_[0];
     	float phi = measurement_pack.raw_measurements_[1];
     	float rho_dot = measurement_pack.raw_measurements_[1];
 
     	// convert to cartesian
       float px = rho * cos(phi);
-      float py = rho * sin(phi);;
+      float py = rho * sin(phi);
 
     	ekf_.x_ << px, py, 0, 0;
 
